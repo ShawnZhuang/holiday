2d5fe04805c55c9519d7f199f06684b063421140
shawn.z <shawn.zhuang@outlook.com>
2022-06-13 18:41:04 +0800

    update repo tools

--- holiday/tasks/os_setup/repo_git_setup.sh
+++ holiday/tasks/os_setup/repo_git_setup.sh
@@ -1,3 +1,4 @@
+SCRIPT_DIR=$(realpath $(dirname $BASH_SOURCE[0]))
 # set
 
 # git config --global user.name "Your Name"
@@ -6,7 +7,8 @@ git config --global core.editor vim
 
 
 USER_BIN_DIR=${HOME}/.local/bin
-curl https://mirrors.tuna.tsinghua.edu.cn/git/git-repo -o ${USER_BIN_DIR}/repo
+# curl https://mirrors.tuna.tsinghua.edu.cn/git/git-repo -o ${USER_BIN_DIR}/repo
+cp ${SCRIPT_DIR}/repo  ${USER_BIN_DIR}/repo
 chmod +x ${USER_BIN_DIR}/repo
 
 
